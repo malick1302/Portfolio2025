40678b44606b11af035f24c7fc75f98dd328ff32
Brice Bammez <bricebammez@MacBook-Pro-de-Brice.local>
2025-03-28 16:26:48 +0100

    test

--- portfolio/src/pages/contacts.jsx
+++ portfolio/src/pages/contacts.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
   import Layout from '../components/layout';
   import EmailForm from '../components/EmailForm';
-  import LogoContact from '../components/logoContact';
 import Navbar from './Navbar';
 
 
@@ -13,7 +12,6 @@ const Contacts = () => {
         <Layout className="w-screen">
           <div className='flex md:flex-col'>
           <Navbar />
-          <LogoContact />
           </div>
           <div className=' lg:ml-40 lg:mr-40'>
           
